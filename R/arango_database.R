a1e7a4f7b783178694abfa1fa07be9d86aceb159
ggalatolo <g.galatolo@kode-solutions.net>
2019-01-25 21:52:44 +0000

    createOnFails support for new database

--- R/arango_database.R
+++ R/arango_database.R
@@ -3,6 +3,16 @@ library(httr)
 library(R6)
 library(magrittr)
 
+.check_connection <- function(.connection){
+  if(is.null(.connection)){
+    stop("Connection is NULL, please provide a valid 'ArangoConnection'")
+  }
+  
+  if(class(.connection)[1] != "ArangoConnection"){
+    stop("Only 'ArangoConnection' objects can be processed by aRango::databases")
+  }
+}
+
 #' Returns all the databases available on the server identified by the current connection 
 #'
 #' @param .connection the server connection to the ArangoDB instance
@@ -11,13 +21,7 @@ library(magrittr)
 #' @author Gabriele Galatolo, g.galatolo(at)kode.srl
 databases <- function(.connection, includeSystem=FALSE){
   
-  if(is.null(.connection)){
-    stop("Connection is NULL, please provide a valid 'ArangoConnection'")
-  }
-  
-  if(class(.connection)[1] != "ArangoConnection"){
-    stop("Only 'ArangoConnection' objects can be processed by aRango::databases")
-  }
+  .check_connection(.connection)
   
   connectionString <- .connection$getConnectionString()
   response <- httr::GET(paste0(connectionString,"/_api/database"))
@@ -35,3 +39,114 @@ databases <- function(.connection, includeSystem=FALSE){
   
   return(databasesList$result)
 }
+
+
+#' Return a wrapper to the given database, if any for the given connection
+#'
+#' @param .connection the server connection to the ArangoDB instance
+#' @param name the name of the database to which connect to, the default is "_system" that is
+#'             the default of ArangoDB
+#' @param createOnFail if the database were not found creates it. If the parameter is set to TRUE
+#'                     and the default database is requested the function fails. Default is FALSE             
+#' @author Gabriele Galatolo, g.galatolo(at)kode.srl
+database <- function(.connection, name="_system", createOnFail=FALSE){
+  .check_connection(.connection)
+  
+  if(is.null(name)){
+    stop("Name of database cannot be NULL")
+  }
+  
+  # if createOnFail try to create the database: if the db exist, then go ahead,
+  # otherwise it is a prerequisite to return the requested db
+  if(createOnFail){
+    databaseInfoRequest <- paste0(.connection$getConnectionString(), "/_api/database")
+  
+    # Waiting for version response
+    response <- httr::POST(databaseInfoRequest, body = list(name=name))
+  }
+  
+  db <- .aRango_database$new(.connection, name, forceCreate = createOnFail)
+  
+  return(db)
+}
+
+
+#' An ArangoDatabase is a class that wraps all the operations and interaction methods with
+#' a database existing within the specific ArangoConnection.
+#'
+#' @author Gabriele Galatolo, g.galatolo(at)kode.srl
+.aRango_database <- R6Class (
+  "ArangoDatabase",
+  
+  public = list(
+    #' Creates a new interface to some database available through the given connection
+    #'
+    #' @param connection an ArangoConnection with an ArangoDB server
+    #' @param dbname the database name to which connect to 
+    #'
+    initialize = function(connection, dbname, forceCreate=FALSE) {
+      
+      if(is.null(connection)){
+        stop("Connection is NULL, please provide a valid 'ArangoConnection'")
+      }
+      
+      if(class(connection)[1] != "ArangoConnection"){
+        stop("Only 'ArangoConnection' objects can be processed by the class ArangoDatabase")
+      }
+      
+      if(is.null(dbname)){
+        stop("dbname is null, please provide a valid database name")
+      }
+      
+      private$connectionStringRequest <- paste0(connection$getConnectionString(), "/_db/", dbname)
+      databaseInfoRequest <- paste0(private$connectionStringRequest, "/_api/database/current")
+      
+      # Waiting for version response
+      response <- httr::GET(databaseInfoRequest)
+      
+      # Check response status
+      if(status_code(response) == 400){
+        stop("Request is invalid")
+      }
+      
+      if(status_code(response) == 404){
+        stop(paste0("Database ", dbname, " not found. Creates it on the server or call the database 
+                    with the optional parameter 'createOnFail=TRUE'"))
+      }
+      
+      # Response is ok, fill the internal state
+      dbInformation <- content(response)
+      private$dbname <- dbname
+      private$isSystem <- dbInformation$result$isSystem
+      private$id <- dbInformation$result$id
+    },
+    
+    #' Returns the name of the db wrapped by this object
+    #' 
+    #' @author Gabriele Galatolo, g.galatolo(at)kode.srl
+    getName = function(){
+      return(private$dbname)
+    },
+    
+    #' Returns TRUE iff this object is connected to a system database, FALSE otherwise
+    #' 
+    #' @author Gabriele Galatolo, g.galatolo(at)kode.srl
+    isSystemDatabase = function(){
+      return(private$isSystem)
+    },
+    
+    #' Returns the identifier of the database within the connected server
+    #' 
+    #' @author Gabriele Galatolo, g.galatolo(at)kode.srl
+    getId = function(){
+      return(private$id)
+    }
+  ),
+  
+  private = list(
+    dbname = NULL,
+    connectionStringRequest = NULL,
+    isSystem = TRUE,
+    id = NULL
+  )
+)
--- tests/testthat/test_database.R
+++ tests/testthat/test_database.R
@@ -52,3 +52,65 @@ with_mock_api({
     })
   })
 })
+
+with_mock_api({
+  test_that("Requests for _system database works correctly", {
+    # given
+    serverResponse <- RJSONIO::toJSON(list(server="arango", version="3.3.19", license="community"))
+    write(serverResponse, file="./localhost-1234/_api/version.json")
+    serverResponse <- RJSONIO::toJSON(list(code="200", error=FALSE, 
+                                           result=list(name="_system", id="1", path="/some/path", isSystem=TRUE)))
+    write(serverResponse, file="./localhost-1234/_db/_system/_api/database/current.json")
+    connection <- aRangodb::connect("localhost", "1234")
+    
+    # when
+    defaultDb <- connection %>% aRangodb::database()
+    
+    # then
+    expect_equal(defaultDb$getName(), "_system")
+    expect_equal(defaultDb$getId(), "1")
+    expect_true(defaultDb$isSystemDatabase())
+  })
+})
+
+with_mock_api({
+  test_that("Requests for existing database works correctly", {
+    # given
+    serverResponse <- RJSONIO::toJSON(list(server="arango", version="3.3.19", license="community"))
+    write(serverResponse, file="./localhost-1234/_api/version.json")
+    serverResponse <- RJSONIO::toJSON(list(code="200", error=FALSE, 
+                                           result=list(name="testdb", id="1121552", path="/some/path", isSystem=FALSE)))
+    write(serverResponse, file="./localhost-1234/_db/testdb/_api/database/current.json")
+    connection <- aRangodb::connect("localhost", "1234")
+    
+    # when
+    defaultDb <- connection %>% aRangodb::database(name = "testdb")
+    
+    # then
+    expect_equal(defaultDb$getName(), "testdb")
+    expect_equal(defaultDb$getId(), "1121552")
+    expect_false(defaultDb$isSystemDatabase())
+  })
+})
+
+with_mock_api({
+  test_that("Requests for not existing database works correctly", {
+    # given
+    serverResponse <- RJSONIO::toJSON(list(code="201", error=FALSE, result=TRUE))
+    write(serverResponse, file="./localhost-1234/_api/database-d782cf-POST.json")
+    serverResponse <- RJSONIO::toJSON(list(server="arango", version="3.3.19", license="community"))
+    write(serverResponse, file="./localhost-1234/_api/version.json")
+    serverResponse <- RJSONIO::toJSON(list(code="200", error=FALSE, 
+                                           result=list(name="example", id="1121552", path="/some/path", isSystem=FALSE)))
+    write(serverResponse, file="./localhost-1234/_db/example/_api/database/current.json")
+    connection <- aRangodb::connect("localhost", "1234")
+    
+    # when
+    defaultDb <- connection %>% aRangodb::database(name = "example", createOnFail = TRUE)
+    
+    # then
+    expect_equal(defaultDb$getName(), "example")
+    expect_equal(defaultDb$getId(), "1121552")
+    expect_false(defaultDb$isSystemDatabase())
+  })
+})
