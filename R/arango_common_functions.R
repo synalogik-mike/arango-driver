4ee67e798595a573a8e720c73735b7e459e79c5e
ggalatolo <g.galatolo@kode-solutions.net>
2019-01-29 15:06:29 +0000

    improved internal structure of drop() function

--- R/arango_common_functions.R
+++ R/arango_common_functions.R
@@ -12,40 +12,55 @@ drop <- function(.element){
   }
   
   if(class(.element)[1] == "ArangoDatabase"){
-    dbPrefixReq <- .element$.__enclos_env__$private$originalConnection
-    response <- httr::DELETE(paste0(dbPrefixReq,"/_api/database/", .element$getName()))
-    
-    if(status_code(response) == 400){
-      stop("Request is invalid")
-    }
-    
-    if(status_code(response) == 403){
-      stop(paste0("Request has not been executed in the '_system' database"))
-    }
-    
-    if(status_code(response) == 404){
-      stop(paste0("Database cannot be found in the server"))
-    }
-    
-    return(TRUE)
+    return(.drop_database(.element))
   }
   else if(class(.element)[1] == "ArangoCollection"){
-    collectionRequest <- .element$.__enclos_env__$private$connectionStringRequest
-    response <- httr::DELETE(collectionRequest)
-    
-    if(status_code(response) == 400){
-      stop("Request is invalid")
-    }
-    
-    if(status_code(response) == 404){
-      stop(paste0("Collection cannot be found in the server"))
-    }
-    
-    return(TRUE)
+    return(.drop_collection(.element))
   }
   else{
     stop("Only 'ArangoDatabase' objects can be processed by aRango::drop")
   }
   
   return(FALSE)
+}
+
+#' Internal function for collection removal
+#'
+#' @author Gabriele Galatolo, g.galatolo(at)kode.srl 
+.drop_collection <- function(.element){
+  collectionRequest <- .element$.__enclos_env__$private$connectionStringRequest
+  response <- httr::DELETE(collectionRequest)
+  
+  if(status_code(response) == 400){
+    stop("Request is invalid")
+  }
+  
+  if(status_code(response) == 404){
+    stop(paste0("Collection cannot be found in the server"))
+  }
+  
+  return(TRUE)
+}
+
+
+#' Internal function for database removal
+#'
+#' @author Gabriele Galatolo, g.galatolo(at)kode.srl 
+.drop_database <- function(.element){
+  dbPrefixReq <- .element$.__enclos_env__$private$originalConnection
+  response <- httr::DELETE(paste0(dbPrefixReq,"/_api/database/", .element$getName()))
+  
+  if(status_code(response) == 400){
+    stop("Request is invalid")
+  }
+  
+  if(status_code(response) == 403){
+    stop(paste0("Request has not been executed in the '_system' database"))
+  }
+  
+  if(status_code(response) == 404){
+    stop(paste0("Database cannot be found in the server"))
+  }
+  
+  return(TRUE)
 }
